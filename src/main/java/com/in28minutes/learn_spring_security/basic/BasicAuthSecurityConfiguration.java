db017056e8e7baf2e69fe74f0e8636693839fa60
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-08 21:03:11 +0900

    Stored user credentials in memory

--- src/main/java/com/in28minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,4 +31,21 @@ public class BasicAuthSecurityConfiguration {
 		
 		return http.build();
 	}
+	
+	@Bean
+	public UserDetailsService userDetailService() {
+		
+		var user = User.withUsername("in28minutes")
+			.password("{noop}dummy")
+			.roles("USER")
+			.build();
+		
+		var admin = User.withUsername("admin")
+			.password("{noop}dummy")
+			.roles("admin")
+			.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
 }
