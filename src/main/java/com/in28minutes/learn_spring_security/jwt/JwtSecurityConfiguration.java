84d2ddeaeae143963d98d612f10cb994d1faf745
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-10 21:19:45 +0900

    Set up JWT Auth with Spring Security and Spring Boot

--- src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,6 +1,11 @@
-package com.in28minutes.learn_spring_security.basic;
+package com.in28minutes.learn_spring_security.jwt;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
@@ -16,18 +21,24 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
+public class JwtSecurityConfiguration {
 	
 	@Bean
 	@Order(SecurityProperties.BASIC_AUTH_ORDER)
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-//		http.formLogin(withDefaults());
 		http.sessionManagement(
 				session -> session.sessionCreationPolicy(
 						SessionCreationPolicy.STATELESS)
@@ -38,26 +49,11 @@ public class BasicAuthSecurityConfiguration {
 		
 		http.headers().frameOptions().sameOrigin();
 		
+		http.oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+		
 		return http.build();
 	}
-	
-// Storing User Credentials in memory
-//	@Bean
-//	public UserDetailsService userDetailService() {
-//		
-//		var user = User.withUsername("in28minutes")
-//			.password("{noop}dummy")
-//			.roles("USER")
-//			.build();
-//		
-//		var admin = User.withUsername("admin")
-//			.password("{noop}dummy")
-//			.roles("admin")
-//			.build();
-//		
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-	
+
 	// Storing User Credentials using JDBC
 	@Bean
 	public DataSource dataSource() {
@@ -95,6 +91,39 @@ public class BasicAuthSecurityConfiguration {
 	@Bean 
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public KeyPair keyPair() {
+		try {
+			var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	
+	}
+	
+	@Bean
+	public RSAKey rsaKey(KeyPair keyPair) {
+		return new RSAKey
+			.Builder((RSAPublicKey) keyPair.getPublic())
+			.privateKey(keyPair.getPrivate())
+			.keyID(UUID.randomUUID().toString())
+			.build();
+	}
+	
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+		var jwkSet = new JWKSet(rsaKey);
+		
+		return (jwkSelector, context) -> jwkSelector.select(jwkSet);		
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+		return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
 	}
 	
 }
